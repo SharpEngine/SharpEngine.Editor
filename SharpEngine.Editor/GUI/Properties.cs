c3334babe2933303b0bbb5306c9696ac5f81073e
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-09-20 12:58:47 +0200

    feat(properties): Base for Entity and Widget

--- SharpEngine.Editor/GUI/Properties.cs
+++ SharpEngine.Editor/GUI/Properties.cs
@@ -1,13 +1,30 @@
 using ImGuiNET;
+using SharpEngine.Core.Entity;
+using SharpEngine.Core.Widget;
 
 namespace SharpEngine.Editor.GUI;
 
 public class Properties: GuiObject
 {
+    public static dynamic? Selected { get; set; }
+
     public override void Render()
     {
         if (ImGui.Begin("Properties"))
         {
+            if (Selected is Widget widget)
+            {
+                ImGui.SetWindowFontScale(1.2f);
+                ImGui.Text($"Nom : {widget.Name}");
+                ImGui.SetWindowFontScale(1f);
+            }
+            else if (Selected is Entity entity)
+            {
+                ImGui.SetWindowFontScale(1.2f);
+                ImGui.Text($"Nom : {entity.Name}");
+                ImGui.SetWindowFontScale(1f);
+                ImGui.Text($"Tag : {entity.Tag}");
+            }
             ImGui.End();
         }
     }
