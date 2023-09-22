55cbfa694acd9b592ca025a6969cbecf7c631624
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-09-22 18:48:09 +0200

    feat(properties): Add TransformComponent

--- SharpEngine.Editor/GUI/PropertiesExtensions/EntityProperties.cs
+++ SharpEngine.Editor/GUI/PropertiesExtensions/EntityProperties.cs
@@ -1,4 +1,5 @@
 using ImGuiNET;
+using SharpEngine.Core.Component;
 using SharpEngine.Core.Entity;
 
 namespace SharpEngine.Editor.GUI.PropertiesExtensions;
@@ -7,9 +8,30 @@ public static class EntityProperties
 {
     public static void DrawProperties(this Entity entity)
     {
-        ImGui.SetWindowFontScale(1.2f);
-        ImGui.Text($"Nom : {entity.Name}");
-        ImGui.SetWindowFontScale(1f);
-        ImGui.Text($"Tag : {entity.Tag}");
+        if (ImGui.CollapsingHeader("Entity", ImGuiTreeNodeFlags.DefaultOpen))
+        {
+            ImGui.Text($"Name : {entity.Name}");
+            ImGui.Text($"Tag : {entity.Tag}");
+        }
+
+        foreach (var component in entity.Components)
+            component.DrawProperties();
+    }
+
+    public static void DrawProperties(this Component component)
+    {
+        if(component is TransformComponent transformComponent)
+            transformComponent.DrawProperties();
+    }
+
+    public static void DrawProperties(this TransformComponent component)
+    {
+        if (ImGui.CollapsingHeader("TransformComponent", ImGuiTreeNodeFlags.DefaultOpen))
+        {
+            BaseProperties.InputVec2("Position", (() => component.Position, x => component.Position = x));
+            BaseProperties.InputVec2("Scale", (() => component.Scale, x => component.Scale = x));
+            BaseProperties.InputFloat("Rotation", (() => component.Rotation, x => component.Rotation = x));
+            BaseProperties.InputInt("ZLayer", (() => component.ZLayer, x => component.ZLayer = x));
+        }
     }
 }
