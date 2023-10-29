4237518bebe30ea059dd83eb6a253036809c2f37
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-10-29 10:14:52 +0100

    feat(explorer): context menu wip

--- SharpEngine.Editor/GUI/AssetsExplorer.cs
+++ SharpEngine.Editor/GUI/AssetsExplorer.cs
@@ -11,6 +11,29 @@ public class AssetsExplorer : GuiObject
     public override void Render()
     {
         if (ImGui.Begin("Assets Explorer"))
+        {
+            if (ImGui.BeginPopupContextItem("context_asset_menu"))
+            {
+                BaseProperties.InputText(
+                    "Directory Name",
+                    (() => _directionName, (name) => _directionName = name)
+                );
+                if (ImGui.Button("Create Directory"))
+                    Directory.CreateDirectory(CurrentPath + "/" + _directionName);
+
+                ImGui.EndPopup();
+            }
+
+            if (CurrentPath != null)
+            {
+                foreach (var path in Directory.EnumerateDirectories(CurrentPath))
+                    ImGui.Button("D-" + path);
+
+                foreach (var path in Directory.EnumerateFiles(CurrentPath))
+                    ImGui.Button("F-" + path);
+            }
+
             ImGui.End();
+        }
     }
 }
